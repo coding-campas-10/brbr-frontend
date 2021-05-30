e14a033728c8dc5371d07ebbb525eeaf07764e27
uglyonlytoday <superjang3357@naver.com>
2021-05-30 23:59:36 +0900

    kakao auth 플랫폼별 설정 완료(빌드 오류 없음)

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,9 @@
 import UIKit
 import Flutter
 import GoogleMaps
+import KakaoSDKCommon
+import KakaoSDKAuth
+import KakaoSDKUser
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -11,5 +14,12 @@ import GoogleMaps
     GMSServices.provideAPIKey("AIzaSyDqYnR0_CdSJGwL7MFPA1rmsHEJePeIjbM")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+    if (AuthApi.isKakaoTalkLoginUrl(url)) {
+      return AuthController.handleOpenUrl(url: url)
+    }
+    return false
   }
 }
